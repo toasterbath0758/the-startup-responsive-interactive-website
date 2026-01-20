7c8161ac9794f7aaf9d45a01c5fcbcd079098133
toasterbath0758 <toasterbath0758@gmail.com>
2026-01-20 14:31:17 +0100

    #3 mobilenav added

---
+++ index.js
@@ -0,0 +1,19 @@
+const hamburgerMenuKnop = document.querySelector('.hamburgerMenuKnop');
+const hamburgerNav = document.querySelector('.hamburgerNav');
+
+const header = document.querySelector('header');
+
+hamburgerMenuKnop.addEventListener('click', function(){
+hamburgerNav.classList.toggle('active');
+hamburgerMenuKnop.classList.toggle('active');
+header.classList.toggle('active');
+})
+
+const hamburgerKnoppen = document.querySelectorAll('.hamburgerKnop');
+
+hamburgerKnoppen.forEach(function (knop) {
+  knop.addEventListener('click', function(){
+    this.classList.toggle('active');
+  })
+
+})
